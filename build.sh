b90e76afcd5fdbe117ced0e967c74bc7f3ea7904
Brian Moon <brian@moonspot.net>
2025-04-27 16:01:41 -0500

    Pull latest versions from raw github files

--- build.sh
+++ build.sh
@@ -9,10 +9,12 @@ function build() {
 
     VERSION=`docker run --rm php:$IMAGE php -r 'echo phpversion();'`
 
+    MINOR=`echo $VERSION | awk -F . {'print $1"."$2'}`
+
     TAGS=""
 
     TAGS="$TAGS -t brianlmoon/php:"`echo $VERSION | awk -F . {'print $1"."$2"."$3'}`
-    TAGS="$TAGS -t brianlmoon/php:"`echo $VERSION | awk -F . {'print $1"."$2'}`
+    TAGS="$TAGS -t brianlmoon/php:$MINOR"
 
     if [ "$2" == "latest" ]
     then
@@ -21,15 +23,20 @@ function build() {
 
     docker buildx build \
         --platform linux/amd64,linux/arm64 \
-        -t brianlmoon/php:$IMAGE \
+        -t brianlmoon/php:`echo $VERSION | awk -F . {'print $1"."$2'}`-fpm-alpine \
         $TAGS \
         --build-arg BASEIMAGE=$IMAGE \
-        --cache-from brianlmoon/php:$IMAGE \
+        --cache-from brianlmoon/php:$MINOR \
         --push \
         .
 }
 
-build 8.1-fpm-alpine && \
-build 8.2-fpm-alpine && \
-build 8.3-fpm-alpine && \
-build 8.4-fpm-alpine latest
+VERSION81=`curl 'https://raw.githubusercontent.com/docker-library/php/refs/heads/master/8.1/alpine3.21/fpm/Dockerfile' -s | fgrep ' PHP_VERSION ' | awk '{print $3}'`
+VERSION82=`curl 'https://raw.githubusercontent.com/docker-library/php/refs/heads/master/8.2/alpine3.21/fpm/Dockerfile' -s | fgrep ' PHP_VERSION ' | awk '{print $3}'`
+VERSION83=`curl 'https://raw.githubusercontent.com/docker-library/php/refs/heads/master/8.3/alpine3.21/fpm/Dockerfile' -s | fgrep ' PHP_VERSION ' | awk '{print $3}'`
+VERSION84=`curl 'https://raw.githubusercontent.com/docker-library/php/refs/heads/master/8.4/alpine3.21/fpm/Dockerfile' -s | fgrep ' PHP_VERSION ' | awk '{print $3}'`
+
+# build $VERSION81-fpm-alpine && \
+build $VERSION82-fpm-alpine && \
+build $VERSION83-fpm-alpine && \
+build $VERSION84-fpm-alpine latest
